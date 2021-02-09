0b9d776ac97f16ae2af1b10051811e663a97d75b
Steven Smail <41452253+ssmail7@users.noreply.github.com>
2021-02-09 13:08:34 -0500

    Add files via upload

---
+++ bracketMatcher.h
@@ -0,0 +1,15 @@
+# ifndef _MBRACKET_
+# define _MBRACKET_
+
+#include <string>
+using namespace std;
+
+bool is_left(char c);
+
+bool is_right(char c);
+
+bool matches(char L, char R);
+
+bool balanced(string str);
+
+#endif
